8359177d47fcb40c8f91a6e46f3bc2c688dbe757
Bhrighu12 <bhrighugupta@gmail.com>
2023-11-23 13:19:43 +0530

    Second Commit

--- SIH_REACT-1--main/src/components/Sign.js
+++ SIH_REACT-1--main/src/components/Sign.js
@@ -1,10 +1,63 @@
 
 
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Sign =()=>{
-    return(
-        <h1>Diets</h1>
-    )
+const Sign = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  });
 
+  const collectData = async () => {
+    console.warn(name, email, password);
+    let result = await fetch("http://localhost:5000/signupp", {
+      method: "post",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    localStorage.setItem("user", JSON.stringify(result));
+    navigate("/");
+  };
+
+  return (
+    <div className="signup">
+      <h1>Sign Up</h1>
+      <input
+        className="inputBox"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter Name"
+      />
+      <input
+        className="inputBox"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter Email"
+      />
+      <input
+        className="inputBox"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter Password"
+      />
+      <button onClick={collectData} className="appButton" type="button">
+        Sign Up
+      </button>
+    </div>
+  );
 };
 export default Sign;
